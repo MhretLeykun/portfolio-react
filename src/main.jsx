ce8fb571eddd18b3d835addd8a91d8354a988160
Mhret Moges <andreasjhon488@gmail.com>
2022-07-10 20:04:23 +0300

    Sample home page done.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Home from './Pages/Home.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
